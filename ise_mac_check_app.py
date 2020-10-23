a74603a7a7dd4abfbb4725d5bed95942c6836524
Andrew Ohanian <ohanianandrew@gmail.com>
2020-10-23 08:31:15 -0500

    Added app, helper function, and templates

---
+++ ise_mac_check_app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+from ise_mac_search import get_endpoints, get_group_id, get_group, get_rejected_endpoints
+import re
+
+app = Flask(__name__)
+result=''
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/send', methods=['POST'])
+def send(result=result):
+    if request.method == 'POST':
+        #Obtain the MAC submitted through the web form
+        mac = request.form['mac']
+        
+    #Check if MAC is formatted properly
+    if re.match(r"^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$", mac):
+        #Check if MAC is rejected first
+        rejected_endpoints = get_rejected_endpoints()
+        if mac in rejected_endpoints:
+            return render_template('fail.html', result=f'MAC {mac} is currenty in a rejected state.')
+
+        #If MAC is not rejected, check if it exists in an approved group
+        else:
+            endpoint_link = get_endpoints(mac=mac)
+        
+        if endpoint_link:
+            group_id = get_group_id(endpoint_link)
+            group_name = get_group(group_id)
+            approved_groups = ['Phones', 'Printers', 'Video_Camers', 'IoT']
+
+            if group_name not in approved_groups:
+                return render_template('home.html', result=f'No match for [{mac}]. Please submit a ticket to add it to the database of approved devices.')
+            else:
+                print(f'Match found, {mac} exists in group: {group_name}')
+                return render_template('home.html', result=f'MAC Found: [{mac}] exists in group [{group_name}]')
+    
+        else:
+            print(f"No match for {mac}")
+            return render_template('home.html', result=f'No match for [{mac}]. Please submit a HEAT ticket to add it to the database of approved devices.')
+    else:
+        return render_template('fail.html', result='Failed MAC formatting check. Please format MAC as 01:23:45:67:89:AB')
+    
+if __name__ == '__main__':
+    app.run(host=0.0.0.0, port=5000)
+
---
+++ ise_mac_search.py
@@ -0,0 +1,65 @@
+import requests
+import json
+requests.packages.urllib3.disable_warnings()
+
+username = 'admin'
+password = 'password'
+auth = (username, password)
+headers = {'Accept': 'application/json'}
+
+def get_rejected_endpoints():
+	'''
+	Returns a list of all currently rejected endpoints
+	'''
+	
+    url = f'https://ise.ngic.com:9060/ers/config/endpoint/getrejectedendpoints'
+
+    response = requests.request("GET", url, auth = auth, headers=headers, verify=False)
+    result = json.loads(response.text)
+
+    return [i['value'] for i in result['OperationResult']['resultValue']]
+    
+
+def get_endpoints(mac):
+	'''
+	Returns the link to the endpoint object of interest, by searching all endpoints and filtering 
+	on the MAC
+	'''
+	
+    url = f'https://ise.ngic.com:9060/ers/config/endpoint?filter=mac.EQ.{mac}'
+	
+    response = requests.request("GET", url, auth = auth, headers=headers, verify=False)
+
+    result = json.loads(response.text)
+    try:
+        return result['SearchResult']['resources'][0]['link']['href']
+    except:
+        return False
+
+def get_group_id(url):
+	'''
+	Returns the group ID the endpoint belongs to
+	'''
+	
+    response = requests.request("GET", url, auth = auth, headers=headers, verify=False)
+
+    result = json.loads(response.text)
+    return result['ERSEndPoint']['groupId']
+
+
+def get_group(group_id):
+	'''
+	Returns the name of the endpoint group
+	'''
+	
+    url = f"https://ise.ngic.com:9060/ers/config/endpointgroup/{group_id}"
+
+    response = requests.request("GET", url, auth = auth, headers=headers, verify=False)
+
+    result = json.loads(response.text)
+    return result['EndPointGroup']['name']
+
+
+
+
+
